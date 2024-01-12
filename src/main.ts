b4eb0781e0e7f7fd6709fd20024813c2cec6b40a
Edwin Gichuhi <edwingichuhis@gmail.com>
2024-01-12 20:25:04 +0300

    Update API title and description, add Swagger documentation for user controller

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('DigiBank API')
+    .setDescription(
+      'API for managing banking operations like deposit, withdraw, and check balance at DigiBank',
+    )
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('bank')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,17 +1,44 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from '../dto/createUserDto';
 import { UserService } from '../services/user.service';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get user by ID' })
+  @ApiResponse({ status: 200, description: 'User found' })
+  @ApiResponse({ status: 404, description: 'User not found' })
   findOne(@Param('id') id: number) {
     return this.userService.findOne(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiBody({
+    type: CreateUserDto,
+    examples: {
+      example1: {
+        value: {
+          name: 'Mercy Elizabeth',
+          idNumber: '12345678',
+          email: 'mercy.liz@example.com',
+          password: 'password123',
+        },
+      },
+      example2: {
+        value: {
+          name: 'Paul Kimono',
+          idNumber: '12345679',
+          email: 'paul.kim@example.com',
+          password: 'password124',
+        },
+      },
+    },
+  })
+  @ApiResponse({ status: 201, description: 'User created successfully' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
