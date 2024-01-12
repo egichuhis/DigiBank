59e76dced286cc62e45094e9ea6202b2657012a8
Edwin Gichuhi <edwingichuhis@gmail.com>
2024-01-12 18:44:55 +0300

    Update imports in auth and user modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,12 +5,12 @@ import { UserService } from 'src/user/services/user.service';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { LocalStrategy } from './strategies/local-strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from 'src/user/models/user.entity';
+import { User } from '../entities/user.entity';
 @Module({
   providers: [AuthService, UserService, JwtService, LocalStrategy],
   controllers: [AuthController],
   imports: [
-    TypeOrmModule.forFeature([UserEntity]),
+    TypeOrmModule.forFeature([User]),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '3600s' },
