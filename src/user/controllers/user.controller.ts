6f5dea40ad3b3a2290501d0136857fc9636d62a4
Edwin Gichuhi <edwingichuhis@gmail.com>
2024-01-12 22:51:53 +0300

    Remove unused import and method in UserController

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,13 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { UserService } from '../services/user.service';
-import { CreateUserDto } from '../dto/createUserDto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
 }
